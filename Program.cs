4d5476dd005e10625063723187c779e0a7224a29
Liam Torres <liam.torreslopez@gmail.com>
2022-10-13 22:30:31 -0700

    switch exercise complete

--- Program.cs
+++ Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "math":
+                    Console.WriteLine("Math is my second language!");
+                    break;
+
+                case "History":
+                    Console.WriteLine("History is one of my developer's favorite genre!!!");
+                    break;
+
+                case "PE":
+                    Console.WriteLine("Having fun and exercise is considered a school subject? Wow I'm amazed!");
+                    break;
+
+                case "Economy":
+                    Console.WriteLine("Economy is a subject that should be prioritized by early ages not until high school!");
+                    break;
+
+                case "English":
+                    Console.WriteLine("English was my creator's least favorite subject but also the one where he had so much fun due to the friendship he had with his teacher");
+                    break;
+
+               
+                    
+
+
+
+            }
+
+            
+
+
+
+
+
+
         }
     }
 }
